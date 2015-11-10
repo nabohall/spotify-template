b2e3dae1bb2aea49fac2ef24260ffa2124347db8
Nick Bohall <nabohall1224@icloud.com>
2015-11-10 08:16:31 -0800

    Made it look a little nicer

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=artist&query='
-var myApp = angular.module('myApp', ['spotify'])
 var countryCode = 'US'
 var possible = "abcdefghijklmnopqrstuvwxyz"
 
-//https://api.spotify.com/v1/search?type=artist&q=Fall%20Out%20Boy - use this is to find ID, for other queries
-myApp.config(function (SpotifyProvider) {
-    SpotifyProvider.setClientId('719c8291eda047a59594190aa2caa468');
-    SpotifyProvider.setRedirectUri('http://www.example.com/callback.html');
-	SpotifyProvider.setScope('');
-	// If you already have an auth token
-	SpotifyProvider.setAuthToken('BQAjb0vcW6c-fFnzMHfHssuWBqquWn2kiB3bOextcqX2KUSQ4GA_2pyBECJmTMbKdb5cxx-EdVIwLo9FWDlJyPYrId93imSTCit85JZrhevGSH9VzYjj5I8fErnlGrpyujpdr479ftk');
-  });
+var myApp = angular.module('myApp', ['spotify'])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http, Spotify) {
+  
+  //The Spotify part of the controller
   $scope.audioObject = {}
 
   var makeRelatedTable = function(relatedArtistArr, artistID) {
-  	var artistLI = $('#'+artistID);
-  	$('#'+artistID + ' ul').remove();
+  	var artistLI = $('.'+artistID);
+  	$('.'+artistID + ' ul').remove();
 
   	var ul = $('<ul>Related Artists</ul');
 
   	angular.forEach(relatedArtistArr, function(artist){
   		var li = $('<li></li>').attr('ng-click','displayInfo($event)');
-  		li.attr('id', artist.id);
+  		li.addClass(artist.id);
+
   		li.text(artist.name);
   		ul.append(li);
   	});
@@ -36,7 +30,9 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http, Spotify) {
   	return Math.floor(Math.random() * (max-min+1)+min);
   }
 
+  //Find random Artist
   $scope.randomize = function() {
+    $('#initial ul').remove();
   	var randChar = possible.charAt(randomNumber(0,possible.length));
 
   	Spotify.search(randChar+'*', 'artist').then(function(response){
@@ -45,7 +41,9 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http, Spotify) {
   	});
   }
 
+  //Get user entered artist
   $scope.getArtist = function() {
+    $('#initial ul').remove();
     Spotify.search($scope.artistQuery,'artist').then(function(response){
     	$scope.artistInitial = response.artists.items[0];
     })
@@ -65,24 +63,23 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http, Spotify) {
   }
 
   $scope.displayInfo = function(artistOb){
-  	var artistID = artistOb.target.attributes.id.value
+    	var artistID = artistOb.target.attributes.class.value
 
-  	Spotify.getArtist(artistID).then(function(artistData){
-	  	$scope.showInfo = true;
-	  	$scope.artistName = artistData.name;
-	  	$scope.artistPop = artistData.popularity;
-	  	$scope.artistImg = artistData.images[0].url;
-	  	$scope.artistGenres = artistData.genres;
-	  	Spotify.getArtistTopTracks(artistData.id, countryCode).then(function(response){
-	  		$scope.topTracks = response.tracks;
-	  	})
+    	Spotify.getArtist(artistID).then(function(artistData){
+  	  	$scope.showInfo = true;
+  	  	$scope.artistName = artistData.name;
+  	  	$scope.artistPop = artistData.popularity;
+  	  	$scope.artistImg = artistData.images[0].url;
+  	  	$scope.artistGenres = artistData.genres;
+  	  	Spotify.getArtistTopTracks(artistData.id, countryCode).then(function(response){
+  	  		$scope.topTracks = response.tracks;
+  	  	})
+    	})
+
+    	Spotify.getRelatedArtists(artistID).then(function(data){
+    		success:makeRelatedTable(data.artists, artistID);
   	})
-
-  	Spotify.getRelatedArtists(artistID).then(function(data){
-  		success:makeRelatedTable(data.artists, artistID);
-	})
   }
-
 
 })
 
